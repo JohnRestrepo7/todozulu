0b0e3bfd5219f3a90216309586ce17ca4f75ee29
JOHN ESTIVEN RESTREPO MARIN <johnrestrepo246155@correo.itm.edu.co>
2021-08-19 21:03:51 -0500

    Add Entity

---
+++ todozulu.Common/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todozulu.Common.Models
+{
+    public class Todo
+    {
+        public DateTime createdTime { get; set; }
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
